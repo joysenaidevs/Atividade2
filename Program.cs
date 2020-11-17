4c23d2ca87a283d0f86ed02d9481cd843b2d5cec
Joyce <joycessilva17@hotmail.com>
2020-11-17 02:29:46 -0300

    atividade dois

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Atividade2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Declaração de variável
+            int idade = 0;
+            int ano = 0; //ano de nascimento
+            int ano2 = ano - 2020;  //ano atual
+            int semanas = (idade * 7);
+
+
+            //Entrada de dados
+            Console.WriteLine("Insira sua idade");
+            idade = int.Parse(Console.ReadLine());
+            Console.WriteLine("Insira seu ano de nascimento");
+            ano = int.Parse(Console.ReadLine());
+
+            //Processamento
+            System.Console.WriteLine("Idade em anos : " + ano);
+            System.Console.WriteLine("Idade em semanas: " + semanas);
+            System.Console.WriteLine("Ano atual: " +ano2);
+        }
+    }
+}
